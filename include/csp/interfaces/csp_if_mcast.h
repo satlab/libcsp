8d0181ada77c3accc3ddcd26b99112b20251ff00
Jeppe Ledet-Pedersen <jlp@satlab.org>
2016-07-20 22:12:25 +0200

    interfaces: mcast: add UDP multicast interface
    This patch adds support for sending CSP messages over UDP multicast
    
    The transmitted CSP packets are encapsulated in UDP datagrams with the
    CSP header in the first 4 bytes. The header is sent in big endian byte
    order.
    
    Currently, the interface uses Linux system calls directly, but support
    for other network stacks can easily be added later without breaking the
    current API.
    
    The multicast socket is setup for loopback mode so multiple applications
    can communicate on one host.
    
    Signed-off-by: Jeppe Ledet-Pedersen <jlp@satlab.com>

---
+++ include/csp/interfaces/csp_if_mcast.h
@@ -0,0 +1,49 @@
+/*
+Cubesat Space Protocol - A small network-layer protocol designed for Cubesats
+Copyright (C) 2016 GomSpace ApS (http://www.gomspace.com)
+Copyright (C) 2016 AAUSAT3 Project (http://aausat3.space.aau.dk)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+ * Multicast UDP interface for CSP
+ * Copyright (C) 2016 Satlab ApS (http://www.satlab.com) 
+ */
+
+#ifndef _CSP_IF_MCAST_H_
+#define _CSP_IF_MCAST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <csp/csp.h>
+#include <csp/csp_interface.h>
+
+extern csp_iface_t csp_if_mcast;
+
+/**
+ * Init multicast interface
+ * @param ifc Interface to bind to for multicast traffic, NULL for any
+ * @return 0 if interface was successfully initialized, -1 otherwise
+ */
+int csp_mcast_init(const char *ifc);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _CSP_IF_MCAST_H_ */
