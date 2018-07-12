7a6b1fa4ca9b6cb6beab2a860ef60aac1956a06d
Jeppe Ledet-Pedersen <jlp@satlab.com>
2018-07-12 09:25:04 +0200

    csp: pass library name to python through environment variable

--- python/cspnode.py
+++ python/cspnode.py
@@ -1,35 +1,28 @@
 # Copyright (c) 2016-2017 Satlab Aps <satlab@satlab.com>
 # All rights reserved. Do not distribute without permission.
 
-from __future__ import print_function
-
-import sys
 import os
-import struct
 import ctypes
 
-def init(testlib_file):
-    global testlib
-    testlib = testlib_file
+# Load test library
+testlib = ctypes.CDLL(os.environ['SL_TESTLIB'])
+
+# void csp_reboot(uint8_t node)
+_csp_reboot = testlib.csp_reboot
+_csp_reboot.argtypes = [ctypes.c_uint8]
+_csp_reboot.restype = None
+
+# int csp_ping(uint8_t node, uint32_t timeout, unsigned int size, uint8_t conn_options)
+_csp_ping = testlib.csp_ping
+_csp_ping.argtypes = [ctypes.c_uint8, ctypes.c_uint32, ctypes.c_uint, ctypes.c_uint8]
+_csp_ping.restype = ctypes.c_int
 
 class CSPNode():
     def __init__(self, addr):
-        # Get function references from lib
-        global testlib
-
-        # void csp_reboot(uint8_t node)
-        self._csp_reboot = testlib.csp_reboot
-        self._csp_reboot.argtypes = [ctypes.c_uint8]
-        self._csp_reboot.restype = None
-
-        self._csp_ping = testlib.csp_ping
-        self._csp_ping.argtypes = [ctypes.c_uint8, ctypes.c_uint32, ctypes.c_uint, ctypes.c_uint8]
-        self._csp_ping.restype = ctypes.c_int
-
         self.addr = addr
 
     def ping(self, timeout=1000, size=1, options=0):
-        return self._csp_ping(self.addr, timeout, size, options)
+        return _csp_ping(self.addr, timeout, size, options)
 
     def reboot(self):
-        self._csp_reboot(self.addr)
+        _csp_reboot(self.addr)
