d7ad401fe69d53b2206e197eefbbf074d65aa63a
Jeppe Ledet-Pedersen <jlp@satlab.com>
2020-05-28 12:56:33 +0200

    csp: include header and CRC in KISS sizes

--- src/interfaces/csp_if_kiss.c
+++ src/interfaces/csp_if_kiss.c
@@ -101,7 +101,7 @@ void csp_kiss_rx(csp_iface_t * interface, uint8_t * buf, int len, void * pxTaskW
 		unsigned char inputbyte = *buf++;
 
 		/* If packet was too long */
-		if (driver->rx_length > interface->mtu) {
+		if (driver->rx_length > interface->mtu + CSP_HEADER_LENGTH + sizeof(uint32_t)) {
 			csp_log_warn("KISS RX overflow");
 			interface->rx_error++;
 			driver->rx_mode = KISS_MODE_NOT_STARTED;
@@ -122,9 +122,9 @@ void csp_kiss_rx(csp_iface_t * interface, uint8_t * buf, int len, void * pxTaskW
 			/* Try to allocate new buffer */
 			if (driver->rx_packet == NULL) {
 				if (pxTaskWoken == NULL) {
-					driver->rx_packet = csp_buffer_get(interface->mtu);
+					driver->rx_packet = csp_buffer_get(interface->mtu + sizeof(uint32_t));
 				} else {
-					driver->rx_packet = csp_buffer_get_isr(interface->mtu);
+					driver->rx_packet = csp_buffer_get_isr(interface->mtu + sizeof(uint32_t));
 				}
 			}
 
