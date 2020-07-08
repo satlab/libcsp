9f614f0536e0caea304ef0e4d1ff6c0b6bbd770d
Jeppe Ledet-Pedersen <jlp@satlab.com>
2020-07-08 12:00:44 +0200

    csp: switch linux usart setup to termios2
    Support custom bitrates by using termios2 interface instead of termios.
    Also let the driver return error if the configuration fails.

--- src/drivers/usart/usart_linux.c
+++ src/drivers/usart/usart_linux.c
@@ -23,11 +23,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <termios.h>
 #include <fcntl.h>
 
 #include <sys/ioctl.h>
 #include <linux/serial.h>
+#include <asm/termbits.h>
 
 #include <csp/csp.h>
 
@@ -42,7 +42,7 @@ static void *serial_rx_thread(void *vptr_args)
 	while (1) {
 		length = read(csp_usart_fd, cbuf, sizeof(cbuf));
 		if (length <= 0) {
-			perror("USART read error");
+			csp_log_error("USART read error");
 			break;
 		}
 		if (csp_usart_callback)
@@ -54,9 +54,8 @@ static void *serial_rx_thread(void *vptr_args)
 
 int csp_usart_init(struct csp_usart_conf *conf)
 {
-	struct termios options;
+	struct termios2 options;
 	pthread_t rx_thread;
-	int brate = 0;
 #ifdef ASYNC_LOW_LATENCY
 	struct serial_struct ss;
 #endif
@@ -65,122 +64,39 @@ int csp_usart_init(struct csp_usart_conf *conf)
 	if (csp_usart_fd < 0)
 		return CSP_ERR_DRIVER;
 
-	switch(conf->baudrate) {
-#ifdef B50
-	case 50:      brate = B50; break;
-#endif
-#ifdef B75
-	case 75:      brate = B75; break;
-#endif
-#ifdef B110
-	case 110:     brate = B110; break;
-#endif
-#ifdef B134
-	case 134:     brate = B134; break;
-#endif
-#ifdef B150
-	case 150:     brate = B150; break;
-#endif
-#ifdef B200
-	case 200:     brate = B200; break;
-#endif
-#ifdef B300
-	case 300:     brate = B300; break;
-#endif
-#ifdef B600
-	case 600:     brate = B600; break;
-#endif
-#ifdef B1200
-	case 1200:    brate = B1200; break;
-#endif
-#ifdef B1800
-	case 1800:    brate = B1800; break;
-#endif
-#ifdef B2400
-	case 2400:    brate = B2400; break;
-#endif
-#ifdef B4800
-	case 4800:    brate = B4800; break;
-#endif
-#ifdef B9600
-	case 9600:    brate = B9600; break;
-#endif
-#ifdef B19200
-	case 19200:   brate = B19200; break;
-#endif
-#ifdef B38400
-	case 38400:   brate = B38400; break;
-#endif
-#ifdef B57600
-	case 57600:   brate = B57600; break;
-#endif
-#ifdef B115200
-	case 115200:  brate = B115200; break;
-#endif
-#ifdef B230400
-	case 230400:  brate = B230400; break;
-#endif
-#ifdef B460800
-	case 460800:  brate = B460800; break;
-#endif
-#ifdef B500000
-	case 500000:  brate = B500000; break;
-#endif
-#ifdef B576000
-	case 576000:  brate = B576000; break;
-#endif
-#ifdef B921600
-	case 921600:  brate = B921600; break;
-#endif
-#ifdef B1000000
-	case 1000000: brate = B1000000; break;
-#endif
-#ifdef B1152000
-	case 1152000: brate = B1152000; break;
-#endif
-#ifdef B1500000
-	case 1500000: brate = B1500000; break;
-#endif
-#ifdef B2000000
-	case 2000000: brate = B2000000; break;
-#endif
-#ifdef B2500000
-	case 2500000: brate = B2500000; break;
-#endif
-#ifdef B3000000
-	case 3000000: brate = B3000000; break;
-#endif
-#ifdef B3500000
-	case 3500000: brate = B3500000; break;
-#endif
-#ifdef B4000000
-	case 4000000: brate = B4000000; break;
-#endif
-	default: return CSP_ERR_DRIVER;
+	/* Get current configuration */
+	if (ioctl(csp_usart_fd, TCGETS2, &options) != 0) {
+		csp_log_error("Error getting USART options");
+		return CSP_ERR_DRIVER;
 	}
 
-	tcgetattr(csp_usart_fd, &options);
-	cfsetispeed(&options, brate);
-	cfsetospeed(&options, brate);
+	/* Configure for 8N1 */
 	options.c_cflag |= (CLOCAL | CREAD);
 	options.c_cflag &= ~PARENB;
 	options.c_cflag &= ~CSTOPB;
 	options.c_cflag &= ~CSIZE;
+	options.c_cflag &= ~CBAUD;
 	options.c_cflag |= CS8;
+	options.c_cflag |= BOTHER;
 	options.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
 	options.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
 	options.c_oflag &= ~(OCRNL | ONLCR | ONLRET | ONOCR | OFILL | OPOST);
 	options.c_cc[VTIME] = 0;
 	options.c_cc[VMIN] = 1;
-	tcsetattr(csp_usart_fd, TCSANOW, &options);
-	if (tcgetattr(csp_usart_fd, &options) == -1) {
-		perror("error setting USART options");
+	options.c_ispeed = conf->baudrate;
+	options.c_ospeed = conf->baudrate;
+
+	/*
+	 * Update configuration. Note that some drivers accept the requested
+	 * baudrate even if is outside achievable range
+	 */
+	if (ioctl(csp_usart_fd, TCSETS2, &options) != 0) {
+		csp_log_error("Error setting USART options");
 		return CSP_ERR_DRIVER;
 	}
-	fcntl(csp_usart_fd, F_SETFL, 0);
 
-	/* Flush old transmissions */
-	tcflush(csp_usart_fd, TCIOFLUSH);
+	/* Clear any file status flags */
+	fcntl(csp_usart_fd, F_SETFL, 0);
 
 #ifdef ASYNC_LOW_LATENCY
 	/* Try to enable low latency */
