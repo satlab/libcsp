3ff330716f3833d0dd25a4f1569d1f2582f5d0b8
Jeppe Ledet-Pedersen <jlp@satlab.com>
2021-06-11 15:12:18 +0200

    csp: usart: linux: cleanup and optimize
    - Remove unused getbaud function
    - Remove unused includes
    - Do not exit the entire application just because a UART read failed
    - Switch setup to use termios2. This allows non-standard baudrates.
    - Try to enable low-latency serial if available
    
    Signed-off-by: Jeppe Ledet-Pedersen <jlp@satlab.com>

--- src/drivers/usart/usart_linux.c
+++ src/drivers/usart/usart_linux.c
@@ -22,16 +22,15 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include <unistd.h>
 #include <errno.h>
-#include <termios.h>
 #include <fcntl.h>
-#include <sys/time.h>
+
+#include <sys/ioctl.h>
+#include <linux/serial.h>
+#include <asm/termbits.h>
 
 #include <csp/csp.h>
-#include <csp/arch/csp_malloc.h>
 #include <csp/arch/csp_thread.h>
 
 typedef struct {
@@ -45,127 +44,19 @@ static void * usart_rx_thread(void * arg) {
 
 	usart_context_t * ctx = arg;
 	const unsigned int CBUF_SIZE = 400;
-	uint8_t * cbuf = malloc(CBUF_SIZE);
+	uint8_t cbuf[CBUF_SIZE];
 
-	// Receive loop
 	while (1) {
 		int length = read(ctx->fd, cbuf, CBUF_SIZE);
 		if (length <= 0) {
 			csp_log_error("%s: read() failed, returned: %d", __FUNCTION__, length);
-			exit(1);
+			break;
 		}
-                ctx->rx_callback(ctx->user_data, cbuf, length, NULL);
+		if (ctx->rx_callback)
+			ctx->rx_callback(ctx->user_data, cbuf, length, NULL);
 	}
 	return NULL;
 }
-
-#if (0) // Unused function and no prototype in public heaaders
-int getbaud(int ifd) {
-	struct termios termAttr;
-	int inputSpeed = -1;
-	speed_t baudRate;
-	tcgetattr(ifd, &termAttr);
-	/* Get the input speed. */
-	baudRate = cfgetispeed(&termAttr);
-	switch (baudRate) {
-	case B0:
-		inputSpeed = 0;
-		break;
-	case B50:
-		inputSpeed = 50;
-		break;
-	case B110:
-		inputSpeed = 110;
-		break;
-	case B134:
-		inputSpeed = 134;
-		break;
-	case B150:
-		inputSpeed = 150;
-		break;
-	case B200:
-		inputSpeed = 200;
-		break;
-	case B300:
-		inputSpeed = 300;
-		break;
-	case B600:
-		inputSpeed = 600;
-		break;
-	case B1200:
-		inputSpeed = 1200;
-		break;
-	case B1800:
-		inputSpeed = 1800;
-		break;
-	case B2400:
-		inputSpeed = 2400;
-		break;
-	case B4800:
-		inputSpeed = 4800;
-		break;
-	case B9600:
-		inputSpeed = 9600;
-		break;
-	case B19200:
-		inputSpeed = 19200;
-		break;
-	case B38400:
-		inputSpeed = 38400;
-		break;
-	case B57600:
-		inputSpeed = 57600;
-		break;
-	case B115200:
-		inputSpeed = 115200;
-		break;
-	case B230400:
-		inputSpeed = 230400;
-		break;
-#if (CSP_MACOSX == 0)
-	case B460800:
-		inputSpeed = 460800;
-		break;
-	case B500000:
-		inputSpeed = 500000;
-		break;
-	case B576000:
-		inputSpeed = 576000;
-		break;
-	case B921600:
-		inputSpeed = 921600;
-		break;
-	case B1000000:
-		inputSpeed = 1000000;
-		break;
-	case B1152000:
-		inputSpeed = 1152000;
-		break;
-	case B1500000:
-		inputSpeed = 1500000;
-		break;
-	case B2000000:
-		inputSpeed = 2000000;
-		break;
-	case B2500000:
-		inputSpeed = 2500000;
-		break;
-	case B3000000:
-		inputSpeed = 3000000;
-		break;
-	case B3500000:
-		inputSpeed = 3500000;
-		break;
-	case B4000000:
-		inputSpeed = 4000000;
-		break;
-#endif
-	}
-
-	return inputSpeed;
-
-}
-#endif
 
 int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length) {
 
@@ -181,33 +72,11 @@ int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length) {
 
 int csp_usart_open(const csp_usart_conf_t *conf, csp_usart_callback_t rx_callback, void * user_data, csp_usart_fd_t * return_fd) {
 
-	int brate = 0;
-	switch(conf->baudrate) {
-		case 4800:    brate=B4800;    break;
-		case 9600:    brate=B9600;    break;
-		case 19200:   brate=B19200;   break;
-		case 38400:   brate=B38400;   break;
-		case 57600:   brate=B57600;   break;
-		case 115200:  brate=B115200;  break;
-		case 230400:  brate=B230400;  break;
-#if (CSP_MACOSX == 0)
-		case 460800:  brate=B460800;  break;
-		case 500000:  brate=B500000;  break;
-		case 576000:  brate=B576000;  break;
-		case 921600:  brate=B921600;  break;
-		case 1000000: brate=B1000000; break;
-		case 1152000: brate=B1152000; break;
-		case 1500000: brate=B1500000; break;
-		case 2000000: brate=B2000000; break;
-		case 2500000: brate=B2500000; break;
-		case 3000000: brate=B3000000; break;
-		case 3500000: brate=B3500000; break;
-		case 4000000: brate=B4000000; break;
+	struct termios2 options;
+	pthread_t rx_thread;
+#ifdef ASYNC_LOW_LATENCY
+	struct serial_struct ss;
 #endif
-		default:
-			csp_log_error("%s: Unsupported baudrate: %u", __FUNCTION__, conf->baudrate);
-			return CSP_ERR_INVAL;
-	}
 
 	int fd = open(conf->device, O_RDWR | O_NOCTTY | O_NONBLOCK);
 	if (fd < 0) {
@@ -215,34 +84,47 @@ int csp_usart_open(const csp_usart_conf_t *conf, csp_usart_callback_t rx_callbac
 		return CSP_ERR_INVAL;
 	}
 
-	struct termios options;
-	tcgetattr(fd, &options);
-	cfsetispeed(&options, brate);
-	cfsetospeed(&options, brate);
+	/* Get current configuration */
+	if (ioctl(fd, TCGETS2, &options) != 0) {
+		csp_log_error("%s: Failed to get attributes on device: [%s], errno: %s", __FUNCTION__, conf->device, strerror(errno));
+		return CSP_ERR_DRIVER;
+	}
+
+	/* Configure for 8N1 */
 	options.c_cflag |= (CLOCAL | CREAD);
 	options.c_cflag &= ~PARENB;
 	options.c_cflag &= ~CSTOPB;
 	options.c_cflag &= ~CSIZE;
+	options.c_cflag &= ~CBAUD;
 	options.c_cflag |= CS8;
+	options.c_cflag |= BOTHER;
 	options.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
 	options.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
 	options.c_oflag &= ~(OCRNL | ONLCR | ONLRET | ONOCR | OFILL | OPOST);
 	options.c_cc[VTIME] = 0;
 	options.c_cc[VMIN] = 1;
-	/* tcsetattr() succeeds if just one attribute was changed, should read back attributes and check all has been changed */
-	if (tcsetattr(fd, TCSANOW, &options) != 0) {
+	options.c_ispeed = conf->baudrate;
+	options.c_ospeed = conf->baudrate;
+
+	/*
+	 * Update configuration. Note that some drivers accept the requested
+	 * baudrate even if is outside achievable range
+	 */
+	if (ioctl(fd, TCSETS2, &options) != 0) {
 		csp_log_error("%s: Failed to set attributes on device: [%s], errno: %s", __FUNCTION__, conf->device, strerror(errno));
-		close(fd);
 		return CSP_ERR_DRIVER;
 	}
+
+	/* Clear any file status flags */
 	fcntl(fd, F_SETFL, 0);
 
-	/* Flush old transmissions */
-	if (tcflush(fd, TCIOFLUSH) != 0) {
-		csp_log_error("%s: Error flushing device: [%s], errno: %s", __FUNCTION__, conf->device, strerror(errno));
-		close(fd);
-		return CSP_ERR_DRIVER;
+#ifdef ASYNC_LOW_LATENCY
+	/* Try to enable low latency */
+	if (ioctl(fd, TIOCGSERIAL, &ss) == 0) {
+		ss.flags |= ASYNC_LOW_LATENCY;
+		ioctl(fd, TIOCSSERIAL, &ss);
 	}
+#endif
 
 	usart_context_t * ctx = calloc(1, sizeof(*ctx));
 	if (ctx == NULL) {
