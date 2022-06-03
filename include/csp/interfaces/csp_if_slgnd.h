a0b18e4cf7f645301ef66209670121e68cdc6027
Jeppe Ledet-Pedersen <jlp@satlab.com>
2022-06-03 14:17:16 +0200

    csp: interfaces: add extern C guards and doc for slgnd/sludp interface
    Signed-off-by: Jeppe Ledet-Pedersen <jlp@satlab.com>

--- include/csp/interfaces/csp_if_slgnd.h
+++ include/csp/interfaces/csp_if_slgnd.h
@@ -7,11 +7,28 @@
 #ifndef _CSP_IF_SLGND_H_
 #define _CSP_IF_SLGND_H_
 
-#include <csp/csp.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <csp/csp_interface.h>
 
+/**
+   Default name of SLGND interface.
+*/
 #define CSP_IF_SLGND_DEFAULT_NAME	"SLGND"
 
+/**
+   Setup SLGND interface.
+   @param[in] radio_host Hostname or IP address of radio host. Append :port to change from the default port.
+   @param[in] ifname Name of CSP interface, use NULL for default name #CSP_IF_SLGND_DEFAULT_NAME.
+   @param[out] ifc Created CSP interface.
+   @return #CSP_ERR_NONE on succcess - else assert.
+*/
 int csp_slgnd_init(const char *radio_host, const char *ifname, csp_iface_t **ifc);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* _CSP_IF_SLGND_H_ */
--- include/csp/interfaces/csp_if_sludp.h
+++ include/csp/interfaces/csp_if_sludp.h
@@ -7,11 +7,28 @@
 #ifndef _CSP_IF_SLUDP_H_
 #define _CSP_IF_SLUDP_H_
 
-#include <csp/csp.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <csp/csp_interface.h>
 
+/**
+   Default name of SLUDP interface.
+*/
 #define CSP_IF_SLUDP_DEFAULT_NAME	"SLUDP"
 
+/**
+   Setup SLUDP interface.
+   @param[in] device Ethernet device to bind and read address prefix from.
+   @param[in] ifname Name of CSP interface, use NULL for default name #CSP_IF_SLUDP_DEFAULT_NAME.
+   @param[out] ifc Created CSP interface.
+   @return #CSP_ERR_NONE on succcess - else assert.
+*/
 int csp_sludp_init(const char *device, const char *ifname, csp_iface_t **ifc);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* _CSP_IF_SLUDP_H_ */
